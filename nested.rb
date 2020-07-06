87ffa0019a8a667940cca714fd08e8ded426b8a3
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-06 15:31:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,7 +103,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-return programmer_hash[:alan_kay][:known_for] << "GUI"
+return programmer_hash[:alan_kay][:known_for] = {"GUI"}
 end
 
 def adding_to_dennis
